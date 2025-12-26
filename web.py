9af9f859a14acebbacab5e7894f21dc6b7eb00a6
Krystian Potaczek <krystian.potaczek07@gmail.com>
2025-12-26 21:41:33 +0100

    Add functionality to add and remove todos in the web app

--- web.py
+++ web.py
@@ -4,10 +4,22 @@ import functions
 
 todos = functions.get_todos()
 
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
 st.title("My Todo App")
 st.write("This app is to increase your productivity.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
-st.text_input(label="", placeholder="Add new todo")
+st.text_input(label="", placeholder="Add new todo...", on_change=add_todo, key='new_todo')
