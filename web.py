3e9c74b95acc333a26ecc5f3b5f9315b315e7c3d
Krystian Potaczek <krystian.potaczek07@gmail.com>
2025-12-26 21:14:02 +0100

    Create a static web page for todo

---
+++ web.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+import functions
+
+todos = functions.get_todos()
+
+st.title("My Todo App")
+st.write("This app is to increase your productivity.")
+
+for todo in todos:
+    st.checkbox(todo)
+
+st.text_input(label="", placeholder="Add new todo")
