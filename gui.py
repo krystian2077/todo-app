8826524e4493d26abdcc95d955fa102e5e7e07bf
Krystian Potaczek <krystian.potaczek07@gmail.com>
2025-12-26 17:45:50 +0100

    Add Complete and Exit buttons to the GUI

--- gui.py
+++ gui.py
@@ -7,9 +7,14 @@ input_box = sg.InputText(tooltip="Enter a to-do", key="todo")
 add_button = sg.Button("Add")
 list_box = sg.Listbox(functions.get_todos(), key='todos', enable_events=True, size=(45, 10))
 edit_button = sg.Button("Edit")
+complete_bottom = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 window = sg.Window('My To-Do App',
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button, complete_bottom],
+                           [exit_button]],
                    font=('Helvetica', 20))
 
 while True:
@@ -34,10 +39,17 @@ while True:
             todos[index] = new_todo + "\n"
             functions.write_todos(todos)
             window['todos'].update(values=todos)
-
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+        case "Exit":
+            break
         case 'todos':
             window['todo'].update(value=values['todos'][0])
-
         case sg.WINDOW_CLOSED:
             break
 
