5855d9a07d990aace2b20da632e11aea8b70a4aa
Krystian Potaczek <krystian.potaczek07@gmail.com>
2025-12-26 19:27:07 +0100

    Add archive extraction functionality with GUI integration

---
+++ experiments/e18.py
@@ -0,0 +1,31 @@
+import FreeSimpleGUI as sg
+
+from experiments.e18_zip_extractor import extract_archive
+
+sg.theme('Black')
+
+label1 = sg.Text("Select archive:")
+input1 = sg.Input()
+choose_button1 = sg.FileBrowse("Choose", key="archive")
+
+label2 = sg.Text("Select archive:")
+input2 = sg.Input()
+choose_button2 = sg.FileBrowse("Choose", key="folder")
+
+extract_button = sg.Button("Extract")
+output_label = sg.Text(key="output", text_color="green")
+
+window = sg.Window('Archive Extractor',
+                   layout=[[label1, input1, choose_button1],
+                           [label2, input2, choose_button2],
+                           [extract_button, output_label]])
+
+while True:
+    event, value = window.read()
+    print(event, value)
+    archive_path = value['archive']
+    dest_dir = value['folder']
+    extract_archive(archive_path, dest_dir)
+    window['output'].update(values=f"Extracted Successfully!")
+
+window.close()
--- gui.py
+++ gui.py
@@ -11,6 +11,7 @@ label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter a to-do", key="todo")
 add_button = sg.Button("Add")
 list_box = sg.Listbox(functions.get_todos(), key='todos', enable_events=True, size=(45, 10))
+
 edit_button = sg.Button("Edit")
 complete_bottom = sg.Button("Complete")
 exit_button = sg.Button("Exit")
